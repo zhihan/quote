7c7d1c17aa631e0514c01708747c0f21a05f438c
Zhi Han <zhi.han@gmail.com>
2015-02-01 20:48:18 -0500

    Added quote

--- src/main/groovy/quote.groovy
+++ src/main/groovy/quote.groovy
@@ -1,17 +1,32 @@
 package me.zhihan.quote
 
+import groovy.transform.CompileStatic
+
 import java.util.List
 
+@CompileStatic
 class Quote {
 	String text
-	String person
-	List<String> tags
-
+	String person  
+	String source
+	List<String> tags 
+	List<String> videos 
+	List<String> links // Links to related web pages 
+ 
 	Boolean containTag(String tag) {
 		return tags.any{it.equals(tag) }
 	}
 
 	Boolean containTagSub(String sub) {
 		return tags.any{it.indexOf(sub) >= 0}
+	}
+
+	Boolean addTag(String t) {
+		if (tags.any{it.equals(t)}) {
+			false
+		} else {
+			tags.add(t)
+			true
+		}
 	}
 }
--- src/test/groovy/quote_test.groovy
+++ src/test/groovy/quote_test.groovy
@@ -7,17 +7,29 @@ import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.*
 
 class QuoteTest {
-	@Test
-	void testTag() {
-		Quote q = new Quote(text: "whatever", tags: ["a", "b"])
-		assertThat(q.containTag("a"), is(true))
-		assertThat(q.containTag("c"), is(false))
-	}
-	@Test
-	void testSubTag() {
-		Quote q = new Quote(text: "whatever", tags: ["funny", "sincere"])
-		assertThat(q.containTagSub("unny"), is(true))
-		assertThat(q.containTagSub("fun"), is(true))
-		assertThat(q.containTagSub("xxx"), is(false))
-	}
+    @Test
+    void testTag() {
+        Quote q = new Quote(text: "whatever", tags: ["a", "b"])
+        assertThat(q.containTag("a"), is(true))
+        assertThat(q.containTag("c"), is(false))
+    }
+    
+    @Test
+    void testSubTag() {
+        Quote q = new Quote(text: "whatever", tags: ["funny", "sincere"])
+        assertThat(q.containTagSub("unny"), is(true))
+        assertThat(q.containTagSub("fun"), is(true))
+        assertThat(q.containTagSub("xxx"), is(false))
+    }
+
+    @Test
+    void testAddTag() {
+        Quote q = new Quote(text: "whatever", tags: ["a", "b"])
+        Boolean isNew = q.addTag("a")
+        assertThat(isNew, equalTo(false))
+        assertThat(q.getTags().size(), equalTo(2))
+        isNew = q.addTag("c")
+        assertThat(isNew, equalTo(true))
+        assertThat(q.getTags().size(), equalTo(3))
+    }
 }
